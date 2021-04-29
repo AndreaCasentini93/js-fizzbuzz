f3fb2e8a5a689548ed5307f262f6f5b45d664716
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-29 13:19:11 +0200

    Exercise Completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// 1) Scrivi un programma che stampi i numeri da 1 a 100
 for ( i=1 ; i<=100 ; i++ ) {
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
@@ -12,12 +11,3 @@ for ( i=1 ; i<=100 ; i++ ) {
     }
 
 }
-
-// 2) Per i multipli di 3 stampa "Fizz" al posto del numero
-
-
-// 3) Per i multipli di 5 stampa "Buzz" al posto del numero
-
-
-// 4) Per i numeri che sono sia multipli di 3 che di 5 stampa FizzBuzz
-
