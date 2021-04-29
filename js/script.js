9b01d670e5beebd26d0fd918936c9d9bc0a58c41
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-29 14:57:01 +0200

    JS Refactoring

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 function functionFizzBuzz() {
 
-    for ( i=1 ; i<=100 ; i++ ) {
+    if (document.getElementById("fizz_buzz").innerHTML == "") {
 
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            document.getElementById("fizz_buzz").innerHTML += "<span id=\"fizzbuzz\">FizzBuzz</span>" + "<br>";
-        } else if (i % 3 == 0) {
-            document.getElementById("fizz_buzz").innerHTML += "<span id=\"fizz\">Fizz</span>" + "<br>";
-        } else if (i % 5 == 0) {
-            document.getElementById("fizz_buzz").innerHTML += "<span id=\"buzz\">Buzz</span>" + "<br>";
-        } else {
-            document.getElementById("fizz_buzz").innerHTML += i + "<br>";
+        for ( i=1 ; i<=100 ; i++ ) {
+
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                document.getElementById("fizz_buzz").innerHTML += "<span id=\"fizzbuzz\">FizzBuzz</span>" + "<br>";
+            } else if (i % 3 == 0) {
+                document.getElementById("fizz_buzz").innerHTML += "<span id=\"fizz\">Fizz</span>" + "<br>";
+            } else if (i % 5 == 0) {
+                document.getElementById("fizz_buzz").innerHTML += "<span id=\"buzz\">Buzz</span>" + "<br>";
+            } else {
+                document.getElementById("fizz_buzz").innerHTML += i + "<br>";
+            }
+        
         }
-    
+
+    } else {
+        document.getElementById("fizz_buzz").innerHTML = "";
     }
 
 }
 
 function refresh() {
 
-    document.getElementById("fizz_buzz").innerHTML = " ";
+    document.getElementById("fizz_buzz").innerHTML = "";
 
 }
