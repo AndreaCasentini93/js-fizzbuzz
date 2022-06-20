ef4d96fb434a27665b93fc09541195000513b297
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2022-06-20 11:49:37 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-function functionFizzBuzz() {
-
+function fizzBuzz() {
     if (document.getElementById("fizz_buzz").innerHTML == "") {
-
         for (var i=1 ; i<=100 ; i++ ) {
-
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 document.getElementById("fizz_buzz").innerHTML += "<li>" + "<span id=\"fizzbuzz\">FizzBuzz</span>" + "</li>";
             } else if (i % 3 == 0) {
@@ -13,15 +10,13 @@ function functionFizzBuzz() {
             } else {
                 document.getElementById("fizz_buzz").innerHTML += "<li>" + i + "</li>";
             }
-        
         }
-
     }
-
 }
 
 function refresh() {
-
     document.getElementById("fizz_buzz").innerHTML = "";
-
 }
+
+document.getElementById("start").addEventListener("click", fizzBuzz);
+document.getElementById("close").addEventListener("click", refresh);
